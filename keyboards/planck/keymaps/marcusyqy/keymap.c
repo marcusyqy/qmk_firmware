c9179212008640802568a9cb8d51c489d127a687
marcusyqy <marcusyqy@gmail.com>
2021-11-07 23:17:33 +0800

    changed mouse buttons + added shift version of {} instead

--- keyboards/planck/keymaps/marcusyqy/keymap.c
+++ keyboards/planck/keymaps/marcusyqy/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,            KC_O,    KC_P,     KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    NAV_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH,  KC_RSFT,
-    KC_LCTL, KC_LCTL, KC_LGUI, LOWER,   KC_LALT, RAISE,   KC_SPC,  KC_ENT,  RALT_T(KC_PSCR), KC_LBRC, KC_RBRC,  KC_RCTL
+    KC_LCTL, KC_LCTL, KC_LGUI, LOWER,   KC_LALT, RAISE,   KC_SPC,  KC_ENT,  RALT_T(KC_PSCR), KC_LCBR, KC_RCBR,  KC_RCTL
 ),
 
 /* Nav
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, KC_MS_U, _______, KC_WH_U, KC_DEL,
     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,
     _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_CAPS,  _______,
-    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______
 ),
 
 /* Raise
