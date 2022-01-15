b01c0ed85d665b4915cc1a09f47a9d5c02205026
Marcus Yong <marcusyqy@gmail.com>
2022-01-16 03:08:37 +0800

    swapped enter and space for the keyboard again... feels more natural for the hand

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/marcusyqy/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/marcusyqy/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  , NAV_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH, KC_RSFT,
                       KC_GRV, KC_DEL,                                                       KC_LBRC,KC_RBRC,
-                                      KC_BSPC, LOWER,                        KC_SPC, KC_ENT,
+                                      KC_BSPC, LOWER,                        KC_ENT, KC_SPC,
                                       KC_LALT, KC_LCTL,                      KC_RCTL,  RALT_T(KC_PSCR),
                                       RAISE, KC_LGUI,                         KC_RGUI, RAISE
   ),
